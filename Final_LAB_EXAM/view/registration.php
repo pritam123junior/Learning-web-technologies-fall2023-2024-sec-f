b9555187ea37b4486da85323cb262d578dcc8c93
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2023-12-02 14:45:31 +0600

    Update registration.php

--- Final_LAB_EXAM/view/registration.php
+++ Final_LAB_EXAM/view/registration.php
@@ -11,15 +11,15 @@
     <form method="post" action="../controller/registrationCheck.php">
         <fieldset>
             <legend>Signup</legend>
-            Full Name : <input type="text" name="fullName" value=""> <br><br>
-            Username :  <input type="text" name="username" value=""> <br><br>
-            Email :     <input type="email" name="email" value=""> <br><br>
-            Password :  <input type="password" name="password" value=""> <br><br>
-            Confirm Password : <input type="password" name="confirmPassword" value=""> <br><br>
+            Full Name : <input type="text" id="uemp_name" name="fullName" value=""> <br><br>
+            Username :  <input type="text"  id="emp_name" "name="username" value=""> <br><br>
+            Email :     <input type="email"  id="email"name="email" value=""> <br><br>
+            Password :  <input type="password" id="emp_pass" name="password" value=""> <br><br>
+            Confirm Password : <input type="password"id="cp" name="confirmPassword" value=""> <br><br>
     
-            Contact No. : <input type="text" name="contactNo" value=""> <br><br>
+            Contact No. : <input type="text" id="emp_cno" name="contactNo" value=""> <br><br>
 
-            <input type="submit" name="submit" value="Sign Up">
+            <input type="submit" name="submit" value="Sign Up" onclick="regcheckvalidation()">
             <input type="reset" name="reset" value="Reset"> <br><br>
 
             Already have an acccount? <a href="login.php">Login here.</a>
