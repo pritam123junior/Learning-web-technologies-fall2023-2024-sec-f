4cd6b014bc0855c85d8504ce13893a38f4074156
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2023-12-02 14:11:03 +0600

    Add files via upload

---
+++ Final_LAB_EXAM/controller/delete.php
@@ -0,0 +1,28 @@
+<?php
+
+    require_once("../model/userModel.php");
+    session_start();
+    
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) 
+    {
+        $userId = $_GET['id'];
+
+        // Call the deleteUser function to delete the user
+        $deleteStatus = deleteUser($userId);
+
+        if ($deleteStatus) 
+        {
+            header("Location:../view/adminHome.php");
+        } 
+        else 
+        {
+            echo "Failed to delete user.";
+        }
+    } 
+    else 
+    {
+        echo "Invalid request.";
+    }
+
+?>
+
