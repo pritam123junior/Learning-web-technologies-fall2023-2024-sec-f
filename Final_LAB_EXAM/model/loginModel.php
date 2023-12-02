85775a8a52ba59dec94305a565d7f5b00c31d480
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2023-12-02 14:06:08 +0600

    Update loginModel.php

--- Final_LAB_EXAM/model/loginModel.php
+++ Final_LAB_EXAM/model/loginModel.php
@@ -59,48 +59,5 @@
         }
     }
 
-    function addAdmin($user)
-    {
-        $username = $user['username'];
-        $password = $user['password'];
-        $email = $user['email'];
-        $fullName = $user['fullName'];
-        $contactNo = $user['contactNo'];
-        
-        $con = getConnection();
-        $sql = "INSERT INTO adminInfo (username, password, email, full_name, contact_no)
-        VALUES ('$username', '$password', '$email', '$fullName','$contactNo')";
-
-        if (mysqli_query($con, $sql))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    function addUser($user)
-    {
-        $username = $user['username'];
-        $password = $user['password'];
-        $email = $user['email'];
-        $fullName = $user['fullName'];
-        $contactNo = $user['contactNo'];
-        
-        $con = getConnection();
-        $sql = "INSERT INTO users (username, password, email, full_name, contact_no)
-        VALUES ('$username', '$password', '$email', '$fullName','$contactNo')";
-
-        if (mysqli_query($con, $sql))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
+    
 ?>
